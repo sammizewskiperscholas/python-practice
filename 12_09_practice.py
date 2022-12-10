31f95f8933ff22ec2d0d572463848c8ff729b653
suneetha-2022 <suneetha.kandimalla@gmail.com>
2022-12-09 21:54:54 -0500

    first commit

--- 12_09_practice.py
+++ 12_09_practice.py
@@ -26,4 +26,4 @@
 #Fibonacci- The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, #starting from 0 and 1. That is,
   #F(0) = 0, F(1) = 1
   #F(n) = F(n - 1) + F(n - 2), for n > 1.
-  #Create a function that accepts any number and will create a sequence based on the fibonacci sequence.
+  #Create a function that accepts any number and will create a sequence based on the fibonacci sequence. 
