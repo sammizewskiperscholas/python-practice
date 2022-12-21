2eb7e8122cd3b6f45de91949bc92c07cf3bbb8f6
Suneetha-2022 <118309496+Suneetha-2022@users.noreply.github.com>
2022-12-21 16:53:36 -0500

    Delete sum_list.py

--- sum_list.py
+++
@@ -1,8 +0,0 @@
-
-#SumTotal - Create a function that takes a list as an argument and returns the sum of all the values in the list.  For example sum_total([1,2,3,4]) should return 10
-def sumlist(lista):
-    sumtotal = 0
-    for i in lista:
-        sumtotal = sumtotal + i
-    return sumtotal
-a = print(sumlist([1, 2, 3,4]))
